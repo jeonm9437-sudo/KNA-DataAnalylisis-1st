341633dc98a319c12ee0f9566967c97d77d5cd09
miso <jeonm9437@gmail.com>
2026-07-19 15:30:20 +0900

    복습1

---
+++ non.py
@@ -0,0 +1,59 @@
+# print("=== 실습 ===")
+# country = str(input("거주하시는 나라: "))
+# city = str(input("거주하시는 도시: "))
+# print(country + "의", city, "에서 거주하시는 군요!")
+
+
+# print("=== 실습2 ===")
+# a = int(input("수: "))
+# b = int(input("수2: "))
+# print("합:", a + b)
+# print("나눗셈:", a / b)
+
+
+# print("=== 실습 ===")
+# x = int(input("x는?: "))
+# y = int(input("y는?: "))
+# average = (x + y) / 2
+# print("60 이상?", average >= 60)
+
+
+print("=== 실습 ===")
+
+word = "temp_sensor"
+print(word[:4])
+
+word = "temp_sensor"
+print(word[5:])
+
+word = "sensor_01"
+print(word[-2:])
+
+word = "PYTHON"
+print(word[::2])
+
+word = "PYTHON"
+print(word[::-1])
+
+
+word = "20250115"
+print(word[:4], word[4:6], word[6:])
+
+print("=== 7/19일 복습 ===")
+# temp = int(input("온도: "))
+# print("80 초과?", temp > 80)
+# temp = int(input("온도: "))
+# print("80 초과?", temp < 80)
+# total = 35000
+# print("총액:", total, "원")
+# temp = int(input("온도: "))
+# print("80 초과?", temp > 80)
+# print("0 이상?", temp >= 0)
+# temp = int(input("온도: "))
+# print("80 미만?", temp < 80)
+# print("100 이상?", temp >= 100)
+x = int(input("점수1: "))
+y = int(input("점수2: "))
+z = int(input("점수3: "))
+avg = (x + y + z) / 3
+print("60 이상?", avg >= 60)
