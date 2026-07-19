c50671b8b4f4fa2afd0cae7d6f2d2dfcd7c1c16d
miso <jeonm9437@gmail.com>
2026-07-19 16:32:05 +0900

    복습2

--- non.py
+++ non.py
@@ -52,8 +52,67 @@ print("=== 7/19일 복습 ===")
 # temp = int(input("온도: "))
 # print("80 미만?", temp < 80)
 # print("100 이상?", temp >= 100)
-x = int(input("점수1: "))
-y = int(input("점수2: "))
-z = int(input("점수3: "))
-avg = (x + y + z) / 3
-print("60 이상?", avg >= 60)
+# x = int(input("점수1: "))
+# y = int(input("점수2: "))
+# z = int(input("점수3: "))
+# avg = (x + y + z) / 3
+# print("60 이상?", avg >= 60)
+# print("=== 1번 설비 측정값 ===")
+# print("온도(℃ ): ", 75)
+# print("진동(mm/s): ", 2.3)
+
+# print("두 측정값의 합: ", 50 + 44)
+# print("온도 변화량: ", 50 - 44)
+# print("평균 온도: ", (50 + 44) / 2)
+
+# print("=== 1번 설비 점검 ===")
+# print("온도(℃): ", 82)
+# print("온도 상승량: ", 82 - 71)
+
+# temp = 25
+# print(temp)
+# print("temp")
+# x = 10
+# print(x)
+# x = 20
+# print(x)
+
+# temp = 25
+# temp = 30
+# print(temp)
+
+# temp = 25
+# print(temp)
+# name = "센서A"
+# print(name)
+# temp = 30
+# print(temp)
+# tmeperature = 25
+# print(tmeperature)
+# my_number = 15
+# print(my_number)
+# mood = "행복"
+# print(mood)
+
+# age = 27
+# label = "나이"
+# print(label)
+# print(age)
+# x = 10
+# print(x)
+# x = x + 5
+# print(x)
+# x = x * 2
+# print(x)
+# a = 100
+# b = a
+# a = 999
+# print(a)
+# print(b)
+# name = "이름"
+# age = 27
+# country = "포항"
+# print(name), print(age), print(country)
+wideth, height = 4, 3
+print(wideth)
+print(height)
