1cb8dd90a7a3654a9e2f74667b10bbaf33942e3f
miso <jeonm9437@gmail.com>
2026-08-06 12:56:02 +0900

    file 찾기 study😻😻

---
+++ data/file_01.py
@@ -0,0 +1,54 @@
+# 기본 내장함수인 open()으로 sample.txt 파일 열기
+# 읽기모드(r)로 utf-8 형식의 변환을 거쳐 읽기로 한다.
+# 가져온 정보(파인 접근 열쇠 / 참조값) f 에 담는다
+f = open("data/sample.txt", "r", encoding="utf-8")
+
+print(type(f).__name__)  # 타입의 이름
+
+# 텍스트 파일 한줄씩 문자열을 만들기
+lines = f.readlines()
+print(lines)
+
+f.close()  # 열었다면 언젠가는 꼭 닫기
+
+# 만약 신경써서 파일닫기(close) 해주기 귀찮다면
+# with open ... as 문법을 쓰는 것도 좋음
+
+with open("data/sample.txt", "r", encoding="utf-8") as f:
+    # 앞으로 이렇게 들여쓰기 된 코드가 끝나면
+    # 파일 접근을 닫습니다(close)
+
+    # 텍스트 파일 한줄씩 문자열을 만들어 리스트 만들기
+    lines = f.readlines()
+    print(lines)
+
+# ===========================================
+# 😻 GPT 방법 😻
+# import os
+
+# file_path = os.path.join(os.path.dirname(__file__), "sample.txt")
+
+# f = open(file_path, "r", encoding="utf-8")
+# print(type(f).__name__)
+
+# print(f.read())
+
+# f.close()
+
+# 쓰기모드(write)로 파일을 새로 만들기
+f = open("data/hello.txt", "w", encoding="utf-8")
+
+# 파일 쓰기에 줄바꿈을 포함하려면 \n을 포함시킴
+f.write("안녕하세요\n")
+# 파일에 tap들여쓰기를 포함하려면
+f.write("\t반갑습니다\n")
+
+f.close()
+
+# 이어쓰기 몸드(append)로 파일에 내용을 추가
+f = open("data/hello.txt", "a", encoding="utf-8")
+
+f.write("맛점하세요\n")
+
+f.close()
+
